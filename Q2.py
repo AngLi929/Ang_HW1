f9d7c1b868cccd4a85563016555681fbb7c2cec0
AngLi929 <ang.li@yale.edu>
2018-01-23 17:17:45 -0500

    Q3

---
+++ Q2.py
@@ -0,0 +1,8 @@
+text = 'I' + 'love' + 'learning' + 'how' + 'to' + 'code' + '!'
+text = 'I' + ' ' + 'love' + ' ' + 'learning' + ' ' + 'how' + ' '+ 'to' + ' ' + 'code' + '!'
+print(text)
+
+x="I don't love learning how to code!"
+
+text=x[:1]+x[7:]
+print(text)

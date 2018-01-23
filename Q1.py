55ceea8f5e7ebccdd878f9d0c576bc3c3cf23287
AngLi929 <ang.li@yale.edu>
2018-01-23 17:16:30 -0500

    Q1

---
+++ Q1.py
@@ -0,0 +1,23 @@
+#x = 17
+
+x = 17
+
+# integer
+y1 = x
+print(type(y1))
+
+#FLOAT
+y2 = float(x)
+print(type(y2))
+
+#string
+y3 = str(x)
+print(type(y3))
+
+#Boolean, '17 > 14'
+y4 = bool(x > 14)
+print('y4 is a ',type(y4))
+print(type(y4))
+
+text="The value of x is", y1
+print(text)
